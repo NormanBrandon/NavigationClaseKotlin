f9e67e4ec28cd07901e834d783ad13ced8329dee
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2022-04-23 12:08:43 -0500

    navigation proyect

---
+++ app/src/main/java/com/nprmanbrandons11/navigation/ui/FragmentNumber1.kt
@@ -0,0 +1,36 @@
+package com.nprmanbrandons11.navigation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.nprmanbrandons11.navigation.R
+import com.nprmanbrandons11.navigation.databinding.FragmentNumber1Binding
+
+
+class FragmentNumber1 : Fragment() {
+
+    lateinit var binding : FragmentNumber1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNumber1Binding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listeners()
+
+    }
+
+    private fun listeners(){
+        binding.btnNavigateFragment2.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentNumber1_to_fragmentNumber2)
+        }
+    }
+}
---
+++ app/src/main/java/com/nprmanbrandons11/navigation/ui/FragmentNumber2.kt
@@ -0,0 +1,34 @@
+package com.nprmanbrandons11.navigation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.nprmanbrandons11.navigation.R
+import com.nprmanbrandons11.navigation.databinding.FragmentNumber2Binding
+
+
+class FragmentNumber2 : Fragment() {
+
+    lateinit var binding: FragmentNumber2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNumber2Binding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listeners()
+    }
+
+    private fun listeners(){
+        binding.btnNavigateFragment1.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentNumber2_to_fragmentNumber1)
+        }
+    }
+}
